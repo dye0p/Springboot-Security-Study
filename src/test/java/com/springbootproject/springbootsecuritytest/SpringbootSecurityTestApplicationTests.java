5c11687679eaaa7adf8b75e505b9f349589efbe3
DongYeop <s01054830455@gmail.com>
2024-06-06 02:33:23 +0900

    initial commit: 프로젝트 생성

---
+++ src/test/java/com/springbootproject/springbootsecuritytest/SpringbootSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootproject.springbootsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
